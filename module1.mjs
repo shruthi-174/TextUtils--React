d1932108c16d1d5bb8df4e4868c607d8c44b6ebf
Shruthi Mohan <68058205+shruthi-174@users.noreply.github.com>
2021-12-29 21:03:38 +0530

    Add files via upload

---
+++ module1.mjs
@@ -0,0 +1,15 @@
+const a="shruthi";
+const b="hi2";
+const c="hello3";
+const d="finish4";
+
+//by default
+export default a;
+
+//export manually
+export {b};
+export {c};
+export {d};
+
+
+
---
+++ module2.mjs
@@ -0,0 +1,5 @@
+import abcd, {b,c,d} from './module1.mjs';
+console.log(abcd);
+console.log(b);
+console.log(c);
+console.log(d);
